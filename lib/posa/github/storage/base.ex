88a629bc59a34923e71a1e1252cf8663ad60dc06
Thomas Burkhalter <burkhalter@puzzle.ch>
2020-10-17 01:26:44 +0200

    WIP: refactor storage base

--- lib/posa/github/storage/base.ex
+++ lib/posa/github/storage/base.ex
@@ -5,19 +5,17 @@ defmodule Posa.Github.Storage.Base do
 
       @default_value %{}
 
-      def start_link(_),        do: Agent.start_link(fn -> @default_value end, name: __MODULE__)
-      def get,                  do: _get(& &1)
-      def get(key),             do: _get(&Map.get(&1, key))
+      # Public Interface
+      def start_link(args),     do: _start_link(args)
       def get_all,              do: _get(&Map.values(&1))
       def get_by([{key, val}]), do: get_all |> _find(key, val)
       def put(data),            do: _update(fn (_) -> data end)
       def put(key, data),       do: _update(&Map.put(&1, key, data))
       def clear,                do: _update(fn(_) -> @default_value end)
-      def sort(list, key, fun), do: list |> Enum.sort_by(& &1[key], fun)
+      def sort(list, key, fun), do: _sort(list, key, fun)
+      def top(list, key, fun),  do: list |> Enum.sort_by(& &1[key], fun)
 
       # Le plumbing
-      defp _get(fun),                   do: Agent.get(__MODULE__, fun)
-      defp _update(fun),                do: Agent.update(__MODULE__, fun)
       defp _find(list, key, val),       do: Enum.find(list, &match?(%{^key => ^val}, &1))
       defp _filter(list, key, val),     do: Enum.filter(list, &match?(%{^key => ^val}, &1))
 
@@ -31,6 +29,22 @@ defmodule Posa.Github.Storage.Base do
         end
         |> create_path(t, memo ++ [h])
       end
+
+      defp _sort(list, key \\ :id, fun \\ &>=/2) do
+        list |> Enum.sort_by(& &1[key], fun)
+      end
+
+      defp _top()
+
+      # Low Level Interaction with the Agent
+      defp _get(fun),      do: Agent.get(__MODULE__, fun)
+      defp _update(fun),   do: Agent.update(__MODULE__, fun)
+      defp _start_link(_), do: Agent.start_link(fn -> @default_value end, name: __MODULE__)
+
+      # Helps with debugging
+      defp _get_struct,   do: _get(& &1)
+      defp _get_key(key), do: _get(&Map.get(&1, key))
+
     end
   end
 end
