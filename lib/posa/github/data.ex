531ab72b2fe61693805cd2f11ca000640b624f11
Thomas Burkhalter <new.mysteryman@gmail.com>
2020-10-26 15:51:09 +0100

    Fix formatter complaints

--- lib/posa/github/data.ex
+++ lib/posa/github/data.ex
@@ -1,11 +1,9 @@
 defmodule Posa.Github.Data do
   alias Posa.Github.Storage.{Organizations, Users, Events}
-  alias Posa.Github.Data.{Organization, User, Event}
-  alias Ecto.Changeset
 
-  def list_orgs, do: orgs
-  def list_users, do: users
-  def list_events, do: events
+  def list_orgs, do: orgs()
+  def list_users, do: users()
+  def list_events, do: events()
 
   def get_org(id), do: org(id: id)
   def get_user(id), do: user(id: id)
@@ -26,8 +24,4 @@ defmodule Posa.Github.Data do
   defp orgs, do: Organizations.get_all()
   defp users, do: Users.get_all()
   defp events, do: Events.all_events()
-
-  defp org_cs(map), do: Organization.changeset(%Organization{}, map)
-  defp user_cs(map), do: User.changeset(%User{}, map)
-  defp event_cs(map), do: Event.changeset(%Event{}, map)
 end
