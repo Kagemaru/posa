f01864588ef121dcf2613ee9806ab96588910e99
Thomas Burkhalter <new.mysteryman@gmail.com>
2022-02-02 01:03:29 +0100

    Improve iex prompt

--- .iex.exs
+++ .iex.exs
@@ -1,22 +1,24 @@
-# import_file_if_available("~/.iex.exs")
+import_file_if_available("~/.iex.exs")
 
-# IO.puts(Mix.env())
+IO.puts(Mix.env())
 
-# color =
-#   if Mix.env() == "prod" do
-#     IO.ANSI.red()
-#   else
-#     IO.ANSI.green()
-#   end
+color =
+  if Mix.env() == "prod" do
+    IO.ANSI.red()
+  else
+    IO.ANSI.green()
+  end
 
-# reset = IO.ANSI.reset()
+prefix = Mix.env()
 
-# IEx.configure(
-#   colors: [enabled: true],
-#   history_size: -1,
-#   default_prompt: "#{color}#{prefix}#{reset}_%prefix(%counter)>",
-#   alive_prompt: "#{color}#{prefix}#{reset}_%prefix(%node)%counter>"
-# )
+reset = IO.ANSI.reset()
+
+IEx.configure(
+  colors: [enabled: true],
+  history_size: -1,
+  default_prompt: "#{color}#{prefix}#{reset}_%prefix(%counter)>",
+  alive_prompt: "#{color}#{prefix}#{reset}_%prefix(%node)%counter>"
+)
 
 alias Posa.Github.Storage
 alias Posa.Github.Data
