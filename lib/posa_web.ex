77e82acf91d67bc68a61af08114216498e3312f0
Thomas Burkhalter <burkhalter@puzzle.ch>
2022-10-21 13:16:14 +0200

    Update elixir, erlang, hex packages and js packages

--- lib/posa_web.ex
+++ lib/posa_web.ex
@@ -82,7 +82,7 @@ defmodule PosaWeb do
       use Phoenix.HTML
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
-      import Phoenix.LiveView.Helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
