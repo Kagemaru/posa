a4558144f2af2125c35a3aa4123bd4415f8c0873
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-03-24 01:21:26 +0100

    Bind on any port in production

--- config/runtime.exs
+++ config/runtime.exs
@@ -86,7 +86,7 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: :any,
       port: port
     ],
     secret_key_base: secret_key_base
